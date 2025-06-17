b9bac63681964c7bbfc0dacef5c283938d1bf1bd
Satyam Sinha <satyam@acuvity.ai>
2025-06-16 17:36:43 -0700

    k8s: add easy script for install

---
+++ deploy/k8s/acuvity/config/configure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./acuctl import -n /orgs/acuvity.ai/apps -A https://api.acuvity.dev acuctl-import-providers.yaml
+./acuctl import -n /orgs/acuvity.ai/apps -A https://api.acuvity.dev acuctl-import.yaml
