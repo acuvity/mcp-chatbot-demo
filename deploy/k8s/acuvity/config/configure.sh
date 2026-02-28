ece8ba98b5ab284e3cf85cb7e9ee4dab61637509
Satyam Sinha <satyam@acuvity.ai>
2026-02-27 17:39:16 -0800

    new import

--- deploy/k8s/acuvity/config/configure.sh
+++ deploy/k8s/acuvity/config/configure.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./acuctl import -n /orgs/acuvity.ai/apps -A https://api.acuvity.dev acuctl-import-providers.yaml
 ./acuctl import -n /orgs/acuvity.ai/apps -A https://api.acuvity.dev acuctl-import.yaml
